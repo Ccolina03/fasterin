294d5b977e96759155f94d7f623887771b3b9aae
Ccolina03 <colinaperu2003@gmail.com>
2023-01-08 08:32:21 -0700

    Adding the Link to each News

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,5 +1,6 @@
 import urlFor from "../lib/urlFor";
 import Image from "next/image";
+import ClientSideRoute from './ClientSideRoute'
 type Props = {
     posts: Post[]; //all array of posts
 };
@@ -8,13 +9,14 @@ function BlogList({posts}: Props) {
    return (
          <div>
             <hr className= "border-[#F7AB0A] mb-10" />
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
                 {/* Posts */}
                 {posts.map((post) => (
-                    <div key={post._id} className='flex flex-col group cursor-pointer'>
+                    <ClientSideRoute key={post._id} route={`post/${post.slug.current}`}>
+                    <div> key={post._id} className='flex flex-col group cursor-pointer'>
                         <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200-ease-out">
                         <Image
-                                className = "object-cover object-left lg:object_center"
+                                className = "object-cover object-left lg:object-center"
                                 src = {urlFor(post.mainImage).url()}
                                 alt = {post.author.name}
                                 fill
@@ -34,17 +36,28 @@ function BlogList({posts}: Props) {
                                     </p>
                                 </div>
                                     {/* Imporant div for image style */}        
-                                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 item-center"> 
+                                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center"> 
 
                                 {post.categories.map(category =>(
-                                        <div className=" bg-[#F7AB0A] texxt-center text-black px-3 py-1 rounded-full text-s, font-semihold ">
+                                        <div className=" bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm, font-semibold ">
                                             <p>{category.title}</p>
                                             </div>))}
                             
                                 </div>
                            </div>
                         </div>
+
+                        {/* using Line clamp to make preview have less description */}
+                        <div className="mt-5 flex-1">
+                            <p className="underline text-lg font-bold">{post.title}</p>
+                            <p className= "line-clamp-2 text-gray-500">{post.description}</p>
+                        </div>
+                        <p className="mt-5 font-bold flex items-center group-hover:underline">
+                            Read Post
+                           
+                        </p>
                     </div>
+                    </ClientSideRoute>
                 ))}
             </div>                
         </div>
