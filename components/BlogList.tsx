6ae642baf9b86b91bfd5be40508d8e46b2b04fa2
Ccolina03 <colinaperu2003@gmail.com>
2023-01-08 10:34:41 -0700

    Adding dynamic pages

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -6,13 +6,15 @@ type Props = {
 };
 
 function BlogList({posts}: Props) {
+    console.log(posts)
    return (
          <div>
             <hr className= "border-[#F7AB0A] mb-10" />
             <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
                 {/* Posts */}
+    
                 {posts.map((post) => (
-                    <ClientSideRoute key={post._id} route={`/post/${post.slug._current}`}>
+                    <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
                     <div className='flex flex-col group cursor-pointer'>
                         <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200-ease-out">
                         <Image
