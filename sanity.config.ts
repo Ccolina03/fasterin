92aa13ee1c982226d43b5c4a8b922dc7a23abeb3
Ccolina03 <colinaperu2003@gmail.com>
2023-01-07 19:23:40 -0700

    backend connection with next-sanity and without env variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,19 +3,17 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-const projectId=process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 
-const dataset=process.env.NEXT_PUBLIC_SANITY_DATASET
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
-  basepath: '/studio',
-  name: 'default',
-  title: 'fasterin_ba',
-  projectId,
-  dataset,
-
-  plugins: [deskTool(), visionTool()],
-
+  basePath: '/studio', 
+  name: 'PAPAFAM_CONTENT_STUDIO',
+  title: 'PAPAFAM Content Studio',
+  projectId: 'zp317s8w', //to change later env var
+  dataset: 'production', //to change later
+  plugins: [deskTool(), visionTool()],  //desktool permits to update info
   schema: {
     types: schemaTypes,
   },
