ab37fc48589e3dce52a755836486a4c83ade4561
Andy Tran <anhquoctran006@gmail.com>
2023-01-08 02:35:57 -0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import {groq} from "next-sanity";
 import {client} from "../lib/sanity.client";
+import category from "../schemas/category";
 
 type Props = {
     params: {
@@ -18,10 +19,10 @@ async function Post({params: {slug}}:Props) {
 `;
 
 const post: Post = await client.fetch(query,{slug});
-return <article>
-    <section>
+return <article className="px-10 pb-28">
+    <section className="space-y-2 border border-[#F7AB0A] text-white">
         <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-            <div className="absolute top-0 w-full h-full opacity-full">
+            <div className="absolute top-0 w-full h-full opacity-10 blur-smp-10">
                 <Image
                     className="object-cover object-center mx-auto"
                     src ={urlFor(post.mainImage).url()}
@@ -29,6 +30,42 @@ return <article>
                     fill
                 />
             </div>
+            <section className="p-5 bg-[#F7AB0A] w-full">
+                <div className="flex flex-col md:flex-row justify-between gap-y-5">
+                    <div>
+                        <h1 className="text-4xl font-extrabold">(post.title)</h1>
+                        <p>
+                            {new Date(post._createdAt).toLocaleDateString("en-US", {
+                                day:"numeric",
+                                month:"long",
+                                year:"numeric",
+                            })}
+                        </p>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <Image
+                            className="rounded-full"
+                            src ={urlFor(post.author.image).url()}
+                            alt={post.author.name}
+                            height={40}
+                            width={40}
+                        />
+                        <div>
+                            <h3 className="text-lg font-bold">{post.author.name}</h3>
+                            <div>{}</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="italic pt-10">{post.description}</h2>
+                    <div className="flex items-center justify-end mt-auto space-x-2">
+                        {post.categories.map((category)=>(
+                            <p key={category._id}>{category.title}</p>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </div>
     </section>
 </article>;
