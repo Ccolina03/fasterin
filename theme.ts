aaba691fc72579be4cdaa50de14fe5d5749f6508
Ccolina03 <colinaperu2003@gmail.com>
2023-01-07 19:47:08 -0700

    Adding color themes

---
+++ theme.ts
@@ -0,0 +1,51 @@
+import build from "next/dist/build";
+import { buildLegacyTheme } from "sanity";
+//theme background
+//customizing color general attributes
+
+const props = {
+    "--my-white": "#fff",
+    "--my-black": "#1a1a1a",
+    "--my-papafam-brand": "#F7AB0A",
+    "--my-red": "#db4437",
+    "--my-yellow": "#fb4b400",
+    "--my-green": "#0f9d58",
+
+};
+
+export const myTheme = buildLegacyTheme({
+    "--black": props["--my-black"],
+    "--white": props["--my-white"],
+
+    "--gray": "#666",
+    "--gray-base": "#666",
+
+    "--component-bg": props["--my-black"],
+    "--component-text-color": props["--my-white"],
+
+    //brand
+    "--brand-primary": props["--my-papafam-brand"],
+    
+//Default Button
+
+    "--default-button-color": "#666",
+    "--default-button-primary-color": props["--my-papafam-brand"],
+    "--default-button-success-color": props["--my-green"],
+    "--default-button-warning-color": props["--my-green"],
+    "--default-button-danger-color": props["--my-yellow"],
+
+//Navbar
+"--main-navigation-color": props["--my-black"],
+"--main-navigation-color--inverted": props["--my-white"],
+
+//State: 
+"--state-success-color": props["--my-green"],
+"--state-warning-color": props["--my-yellow"],
+"--state-danger-color": props["--my-red"],
+"--state-info-color": props["--my-papafam-brand"],
+
+
+"--focus-color": props["--my-papafam-brand"],
+    
+    
+});
