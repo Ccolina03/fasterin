ab2ee8c3e49caea80a6e1285facd56e639563e65
Ccolina03 <colinaperu2003@gmail.com>
2023-01-09 16:44:35 -0700

    Changing some Details

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ function Banner() {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
         <div>
-            <h1 className="text-7xl">HackPros Daily Blog</h1> 
+            <h1 className="text-7xl">Carlitos Blog</h1> 
             <h2 className="mt-5 md:mt-0">
                 Welcome to {" "}
                 <span className="underline decoration-4 decoration-[#F7AB0A]">
@@ -12,7 +12,7 @@ function Banner() {
             </h2>  
         </div>
         <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
-            Computer Engineering | University of Alberta | The weekly debugging nightmares & More!
+            Life as a Computer Engineer | University of Alberta | The weekly debugging nightmares & More!
         </p>
     </div>
   )
